c8681103dbf01a7d597a51cfcca2dde75afd0478
Cedars22 <qwertydars@hotmail.com>
2026-03-18 14:36:06 -0600

    domain: PostDisplay entitiy

---
+++ packages/domain/lib/src/post/entities/post_display.dart
@@ -0,0 +1,83 @@
+import 'package:equatable/equatable.dart';
+
+class PostDisplay extends Equatable {
+  const PostDisplay({
+    required this.postId,
+    required this.title,
+    required this.content,
+    required this.postCreatedAt,
+    required this.postUpdatedAt,
+    required this.authorId,
+    required this.authonUsername,
+    required this.authorRole,
+    required this.likesCount,
+    required this.commentsCount,
+    required this.currentUserLiked,
+    this.imageUrl,
+    this.authorAvatarUrl,
+  });
+
+  final String postId;
+  final String title;
+  final String content;
+  final String? imageUrl;
+  final DateTime postCreatedAt;
+  final DateTime postUpdatedAt;
+  final String authorId;
+  final String authonUsername;
+  final String? authorAvatarUrl;
+  final String authorRole;
+  final int likesCount;
+  final int commentsCount;
+  final bool currentUserLiked;
+
+  @override
+  List<Object?> get props => [
+    postId,
+    title,
+    content,
+    imageUrl,
+    postCreatedAt,
+    postUpdatedAt,
+    authorId,
+    authonUsername,
+    authorAvatarUrl,
+    authorRole,
+    likesCount,
+    commentsCount,
+    currentUserLiked,
+  ];
+  PostDisplay copyWith({
+    String? postId,
+    String? title,
+    String? content,
+    String? Function()? imageUrl,
+    DateTime? postCreatedAt,
+    DateTime? postUpdatedAt,
+    String? authorId,
+    String? authonUsername,
+    String? Function()? authorAvatarUrl,
+    String? authorRole,
+    int? likesCount,
+    int? commentsCount,
+    bool? currentUserLiked,
+  }) {
+    return PostDisplay(
+      postId: postId ?? this.postId,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      imageUrl: imageUrl != null ? imageUrl() : this.imageUrl,
+      postCreatedAt: postCreatedAt ?? this.postCreatedAt,
+      postUpdatedAt: postUpdatedAt ?? this.postUpdatedAt,
+      authorId: authorId ?? this.authorId,
+      authonUsername: authonUsername ?? this.authonUsername,
+      authorAvatarUrl: authorAvatarUrl != null
+          ? authorAvatarUrl()
+          : this.authorAvatarUrl,
+      authorRole: authorRole ?? this.authorRole,
+      likesCount: likesCount ?? this.likesCount,
+      commentsCount: commentsCount ?? this.commentsCount,
+      currentUserLiked: currentUserLiked ?? this.currentUserLiked,
+    );
+  }
+}
