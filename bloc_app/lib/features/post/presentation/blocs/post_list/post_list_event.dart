3338243028ab1a9d587969469d09a9cb71571e39
Cedars22 <qwertydars@hotmail.com>
2026-03-20 12:21:15 -0600

    presentation: PostListBloc

---
+++ bloc_app/lib/features/post/presentation/blocs/post_list/post_list_event.dart
@@ -0,0 +1,9 @@
+part of 'post_list_bloc.dart';
+
+sealed class PostListEvent {}
+
+final class PostListFetched extends PostListEvent {}
+
+final class PostLitstNextPageFetched extends PostListEvent {}
+
+final class PostListRefreshed extends PostListEvent {}
